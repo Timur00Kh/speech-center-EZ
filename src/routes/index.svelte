c4862e214c4eb6d5f5bc7af0b63b9043ef07560b
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-07 03:52:01 +0300

    add favicon

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,9 +1,13 @@
 <script>
 	import jpg1 from 'images/main/1.jpg';
-	import jpg1 from 'images/main/1.jpg';
+	import jpg2 from 'images/main/2.jpg';
+	import jpg3 from 'images/main/3.jpg';
+	import jpg4 from 'images/main/4.jpg';
+
 </script>
 
 <svelte:head>
-	<title>Sapper project template</title>
+	<title>Речевой центр Эльвиры Зайцевой</title>
+	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
 </svelte:head>
 
