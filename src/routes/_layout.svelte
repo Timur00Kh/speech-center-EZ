345a2480230d2d0c6373f91ce348c140bef2a1a7
Timur00Kh <32297275+Timur00Kh@users.noreply.github.com>
2021-01-07 03:35:50 +0300

    add images and text

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -17,6 +17,10 @@
 
 <Nav {segment}/>
 
+<svelte:head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</svelte:head>
+
 <main>
 	<slot></slot>
 </main>
